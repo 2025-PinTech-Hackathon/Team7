13cd324859eb34c4439546fa8964b600b5e437ee
yesol0442 <yesol0442@kw.ac.kr>
2025-05-14 22:26:40 +0900

    계좌입력화면 수정

--- app/src/main/java/com/example/helperapp_hackathon_team7/InputAccount.java
+++ app/src/main/java/com/example/helperapp_hackathon_team7/InputAccount.java
@@ -1,16 +1,14 @@
 package com.example.helperapp_hackathon_team7;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InputAccount extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.input_account);
+        setContentView(R.layout.activity_input_account);
 
     }
 }
